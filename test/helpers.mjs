df97353c3efd5c57d8f0f09cc40c9993cf84aff8
alejandrobarreda23-blip <alejandrobarreda23@gmail.com>
2026-09-25 22:46:21 +0200

    Adapta helpers.mjs a Knowledge Runtime

--- test/helpers.mjs
+++ test/helpers.mjs
@@ -1,3 +1,6 @@
 import { readFileSync } from 'node:fs';
+import { loadKnowledgeRuntime } from '../src/core/knowledge/node-loader.mjs';
+
 export const ejemplo = (n) => JSON.parse(readFileSync(new URL(`../ejemplos/${n}.json`, import.meta.url), 'utf8'));
 export const copia = (x) => JSON.parse(JSON.stringify(x));
+export const knowledge = loadKnowledgeRuntime();
